35836a8ceb911e531af680303cb4f1e7d76817ca
karan-bst <karanmunjal@Karans-MacBook-Pro.local>
2021-12-28 17:00:24 +0530

    commit message

---
+++ eks-cluster.tf
@@ -0,0 +1,124 @@
+#
+# EKS Cluster Resources
+#  * IAM Role to allow EKS service to manage other AWS services
+#  * EC2 Security Group to allow networking traffic with EKS cluster
+#  * EKS Cluster
+#
+
+resource "aws_iam_role" "demo-cluster" {
+  name = "terraform-eks-demo-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.demo-cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.demo-cluster.name
+}
+
+resource "aws_security_group" "demo-cluster" {
+  name        = "terraform-eks-demo-cluster"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.demo.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-eks-demo"
+  }
+}
+
+resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
+  cidr_blocks       = [local.workstation-external-cidr]
+  description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.demo-cluster.id
+  to_port           = 443
+  type              = "ingress"
+}
+
+resource "aws_eks_cluster" "demo" {
+  name     = var.cluster-name
+  role_arn = aws_iam_role.demo-cluster.arn
+
+  vpc_config {
+    security_group_ids = [aws_security_group.demo-cluster.id]
+    subnet_ids         = aws_subnet.demo[*].id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.demo-cluster-AmazonEKSVPCResourceController,
+  ]
+}
+
+data "tls_certificate" "demo" {
+  url = aws_eks_cluster.demo.identity.0.oidc.0.issuer
+}
+resource "aws_iam_openid_connect_provider" "demo" {
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = []
+  url = aws_eks_cluster.demo.identity.0.oidc.0.issuer
+  
+}
+
+data "aws_iam_policy_document" "example_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.demo.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.demo.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "demo" {
+  assume_role_policy = data.aws_iam_policy_document.example_assume_role_policy.json
+  name               = "demo"
+}
+
+
+resource "aws_eks_identity_provider_config" "demo" {
+  cluster_name = var.cluster-name
+    oidc {
+        client_id = "${substr(aws_eks_cluster.demo.identity.0.oidc.0.issuer, -32, -1)}"
+        identity_provider_config_name = "sdemonew"
+        issuer_url =  "https://.${aws_iam_openid_connect_provider.demo.url}"
+      
+        }  
+       
+    
+   
+}
